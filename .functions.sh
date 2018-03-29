ea7292395118c6a3ea70128d0b43eaf326d323ed
Mariano Anaya <marianoanaya@gmail.com>
2018-03-29 09:53:40 +0200

    New helper for removing old branches

--- .functions.sh
+++ .functions.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/zsh
+#### functions.sh
+## Extra functions available at the command line
+####
 
 workon() {
     ###
@@ -9,4 +12,15 @@ workon() {
         echo "Virtual environment '$1' doesn't exist";
     fi
     source $VENV_LOCATION
+}
+
+
+gitclean() {
+    ####
+    ### A git helper function
+    ### Update ``master`` and delete old (merged) branches
+    ####
+    git checkout master \
+        && git pull origin master \
+        && git branch --merged | egrep -v '(\*|master)' | xargs git branch -d
 }
